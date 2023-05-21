a122d288493b9eeb27ffe6a41d87934662daf7b9
Raybu <raymydrive@gmail.com>
2023-05-21 15:22:47 -0300

    update pagina home e sobre
    iniciando pagina sobre

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let ul = document.querySelector('nav .ul');
+
+function openMenu(){
+    ul.classList.add('open') //metodo add server para adicionar algo à classe
+}
+
+function closeMenu(){
+    ul.classList.remove('open')
+}
